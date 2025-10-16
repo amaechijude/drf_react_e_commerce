6e95b900d6e6ac1f99c1ded357164f02a802f4a8
amaechijude <ugwuamaechi5@gmail>
2025-10-16 14:52:31 +0100

    feat: Initialise backend project with django Created a custom user with custom user manager Started a vite project for the frontend

---
+++ backend/core/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email:str, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_customer', False) # Superuser is not a customer
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    user_id = models.BigAutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=1024)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    is_customer = models.BooleanField(default=True)
+    is_vendor = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField()
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+    
+    class Meta:
+        verbose_name = "CustomUser"
+        verbose_name_plural = "CustomUsers"
+        db_table = "custom_users"
+
+        index = [
+            models.Index(fields=["user_id"]),
+            models.Index(fields=["email"])
+        ]
